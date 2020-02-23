34fc02e5a12067cf4534e103018ef95119f99a8b
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-23 13:02:13 +0900

    セッターインジェクションからコンストラクターインジェクションへ変更

--- src/main/java/com/example/service/CustomerService.java
+++ src/main/java/com/example/service/CustomerService.java
@@ -2,7 +2,6 @@ package com.example.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.domain.Customer;
@@ -10,8 +9,11 @@ import com.example.repository.CustomerRepository;
 
 @Service
 public class CustomerService {
-    @Autowired
     CustomerRepository customerRepository;
+
+    CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public Customer save(Customer customer) {
         return customerRepository.save(customer);
